1bb134fcd5927a3b98fa90e2a1d210a325abb05a
Ibrahim Umar <ibrahim.umar@uit.no>
2020-03-31 22:47:53 +0200

    Improve checktag.sh for handling multiple CI jobs

--- scripts/checktag.sh
+++ scripts/checktag.sh
@@ -17,10 +17,18 @@ IS_TAG=$(git show-ref --tags $TAG | cut -d" " -f1)
 if [[ -z ${IS_TAG} ||  $HEAD_COMMIT == $IS_TAG ]]; then
     echo "This version is not tagged yet or tagged in the current commit, we can always proceed."
 else
-    echo "We need to add sub-version increment"
-    # Find how many development version are there
-    NUM_TAG=$(git show-ref --tags | grep -F $PKGVER | wc -l)
-    NUM_TAG=$((NUM_TAG + 9000))
+    # Find any existing tags
+    EXISTING_TAG=$(git tag --points-at $HEAD_COMMIT)
+
+    if [[ -z $EXISTING_TAG ]]; then
+        echo "We need to add sub-version increment"
+        # Find how many development version are there
+        NUM_TAG=$(git show-ref --tags | grep -F $PKGVER | wc -l)
+        NUM_TAG=$((NUM_TAG + 9000))
+    else
+        NUM_TAG=$(echo $EXISTING_TAG | sed 's/${PKGNAME}-v//g')
+    fi
+
     echo "New version is $NUM_TAG"
     sed -i -e "s/Version: *\([^ ]*\)/Version: ${PKGVER}.${NUM_TAG}/g" DESCRIPTION
 fi

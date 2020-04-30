43c84731df3e1e8063a3062871075fc61da8eebc
Ibrahim Umar <ibrahim.umar@uit.no>
2020-04-30 20:53:34 +0200

    Fix checktag.sh

--- scripts/checktag.sh
+++ scripts/checktag.sh
@@ -3,8 +3,8 @@
 # We automatically increment the package version for every successful pull request that merged to master
 # This happens only if the current package version is already released (tagged)
 
-PKGVER=$(sed -n "s/Version: *\([^ ]*\)/\1/p" DESCRIPTION)
-PKGNAME=$(sed -n "s/Package: *\([^ ]*\)/\1/p" DESCRIPTION)
+PKGVER=$(/usr/bin/sed -n "s/Version: *\([^ ]*\)/\1/p" DESCRIPTION)
+PKGNAME=$(/usr/bin/sed -n "s/Package: *\([^ ]*\)/\1/p" DESCRIPTION)
 TAG=${PKGNAME}-v${PKGVER}
 
 echo "Looking for tag $TAG"
@@ -12,7 +12,7 @@ echo "Looking for tag $TAG"
 git fetch --tags
 
 HEAD_COMMIT=$(git rev-parse HEAD)
-IS_TAG=$(git show-ref --tags $TAG | cut -d" " -f1)
+IS_TAG=$(git show-ref --tags $TAG | /usr/bin/cut -d" " -f1)
 
 if [[ -z ${IS_TAG} ||  $HEAD_COMMIT == $IS_TAG ]]; then
     echo "This version is not tagged yet or tagged in the current commit, we can always proceed."
@@ -23,15 +23,15 @@ else
     if [[ -z $EXISTING_TAG ]]; then
         echo "We need to add sub-version increment"
         # Find how many development version are there
-        NUM_TAG=$(git show-ref --tags | grep -F $PKGVER | wc -l)
+        NUM_TAG=$(git show-ref --tags | /usr/bin/grep -F $PKGVER | /usr/bin/wc -l)
         NUM_TAG=$((NUM_TAG + 9000))
         NEW_VER=${PKGVER}.${NUM_TAG}
     else
         echo "Using existing development tag $EXISTING_TAG"
-        NEW_VER=$( echo "$EXISTING_TAG" | sed "s/${PKGNAME}-v//g")
+        NEW_VER=$(echo "$EXISTING_TAG" | /usr/bin/sed "s/${PKGNAME}-v//g")
     fi
 
     echo "New version is $NEW_VER"
-    sed -i -e "s/Version: *\([^ ]*\)/Version: ${NEW_VER}/g" DESCRIPTION
+    /usr/bin/sed -i -e "s/Version: *\([^ ]*\)/Version: ${NEW_VER}/g" DESCRIPTION
 fi
 

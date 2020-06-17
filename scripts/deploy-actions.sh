a5cabc088dfb0dcb78c6410d1df5a47b76201dcc
Ibrahim Umar <ibrahim.umar@imr.no>
2020-06-17 10:44:12 +0200

    Update deploy script

--- scripts/deploy-actions.sh
+++ scripts/deploy-actions.sh
@@ -14,25 +14,24 @@ addToDrat(){
   ## Get drat repo
   git remote add upstream "https://x-access-token:${DRAT_DEPLOY_TOKEN}@github.com/StoXProject/repo.git"
 
-  # To prevent race condition, set a loop of adding file with Drat
+  # To prevent race condition, set a loop of adding and pushing file with Drat
   RET=1
-  until [ ${RET} -eq 0 ]; do
-    git fetch upstream 2>err.txt
+  until [ $RET -eq 0 ]; do
+    echo "Begin insert"
+    git fetch upstream
     git checkout -f gh-pages
-
     cd ..
-
     Rscript -e "library(drat); insertPackage('./$PKG_FILE', \
       repodir = './drat', \
-      commit='Repo update ${PKG_FILE_PREFIX}: build $BUILD_NUMBER')"
-
+      commit=FALSE)"
     cd drat
-
-    git push 2>err.txt
-    RET=$?
+    git add .
+    git status
+    git commit -m "Add ${PKG_FREL}: build ${BUILD_NUMBER}"
+    git push && RET=$? || RET=$?
     sleep 1
+    echo "End insert"
   done
-
   cd ..
 }
 

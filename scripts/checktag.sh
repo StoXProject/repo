d03c380d3e60201ec28ec1eb65788b6a7a5f78c7
Arne Johannes Holmin <arnejh@hi.no>
2022-08-12 22:18:18 +0200

    Auto-increment disabled. This should be done with RstoxBuild instead.

--- scripts/checktag.sh
+++ scripts/checktag.sh
@@ -16,26 +16,28 @@ IS_TAG=$(git show-ref --tags $TAG | /usr/bin/cut -d" " -f1)
 
 if [[ -z ${IS_TAG} ||  $HEAD_COMMIT == $IS_TAG ]]; then
     echo "This version is not tagged yet or tagged in the current commit, we can always proceed."
-    export FINAL_TAG=$TAG
 else
-    # Find any existing tags
-    EXISTING_TAG=$(git tag --points-at $HEAD_COMMIT)
-
-    if [[ -z $EXISTING_TAG ]]; then
-        echo "We need to add sub-version increment"
-        # Find how many development version are there
-        NUM_TAG=$(git show-ref --tags | /usr/bin/grep -F $PKGVER | /usr/bin/wc -l)
-        NUM_TAG=$((NUM_TAG + 9000))
-        NEW_VER=${PKGVER}.${NUM_TAG}
-    else
-        echo "Using existing development tag $EXISTING_TAG"
-        NEW_VER=$(echo "$EXISTING_TAG" | /usr/bin/sed "s/${PKGNAME}-v//g")
-    fi
-
-    echo "New version is $NEW_VER"
-    /usr/bin/sed -i -e "s/Version: *\([^ ]*\)/Version: ${NEW_VER}/g" DESCRIPTION
-
-    export FINAL_TAG=${PKGNAME}-v${NEW_VER}
+    ## Find any existing tags
+    #EXISTING_TAG=$(git tag --points-at $HEAD_COMMIT)
+    #
+    #if [[ -z $EXISTING_TAG ]]; then
+    #    echo "We need to add sub-version increment"
+    #    # Find how many development version are there
+    #    NUM_TAG=$(git show-ref --tags | /usr/bin/grep -F $PKGVER | /usr/bin/wc -l)
+    #    NUM_TAG=$((NUM_TAG + 9000))
+    #    NEW_VER=${PKGVER}.${NUM_TAG}
+    #else
+    #    echo "Using existing development tag $EXISTING_TAG"
+    #    NEW_VER=$(echo "$EXISTING_TAG" | /usr/bin/sed "s/${PKGNAME}-v//g")
+    #fi
+    #
+    #echo "New version is $NEW_VER"
+    #/usr/bin/sed -i -e "s/Version: *\([^ ]*\)/Version: ${NEW_VER}/g" DESCRIPTION
+    #
+    #export FINAL_TAG=${PKGNAME}-v${NEW_VER}
+    echo "Auto-increment disabled. This should be done with RstoxBuild instead."
 fi
+
+export FINAL_TAG=$TAG
 
 echo "Final tag is $FINAL_TAG"
